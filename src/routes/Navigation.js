b4c07c35aaaad328c8b1f32c9b0c8f9a5747630a
Ju minhark <wnalsgkr@gmail.com>
2021-11-30 15:00:52 +0900

    components

--- src/routes/Navigation.js
+++ src/routes/Navigation.js
@@ -1,18 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 export default function Navigation() {
-  return (
-    <header>
-      <img className="logo" src="../imgages/logo.svg" alt="logo" />
-      <nav>
-        <ul className="nav__links">
-          <li><Link className="nav__links" to='/'>Home</Link></li>
-          <li><Link className="nav__links" to='/about'>About</Link></li>
-          <li><Link className="nav__links" to='/MainConcept'>MainConcept</Link></li>
-        </ul>
-      </nav>
-    </header>
-  )
+	return (
+		<header>
+			<img className="logo" src="../images/logo.svg" alt="logo" />
+			<nav>
+				<ul className="nav__links">
+					<li>
+						<Link className="nav__links" to="/">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link className="nav__links" to="/about">
+							About
+						</Link>
+					</li>
+					<li>
+						<Link className="nav__links" to="/MainConcept">
+							MainConcept
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
